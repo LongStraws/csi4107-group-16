d7bf148e17fcb3272da035a54454f87c48ba95ff
kevin-govier <kevo.p.g@icloud.com>
2026-02-28 19:47:43 -0500

    Added assignment 1 code

--- Assignment-2/cbert.py
+++ Assignment-2/cbert.py
@@ -7,6 +7,7 @@ model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 
 def rerank(query, docTexts, docIDs):
 
+    #Gets query-document pairs for the cross encoder BERT and sorts the results by descending score
     inputs = [(query, docTexts[docID]) for docID in docIDs]
     scores = model.predict(inputs)
     ranked = [docIDs[i] for i in np.argsort(-scores)]
--- Assignment-2/sbert.py
+++ Assignment-2/sbert.py
@@ -9,10 +9,12 @@ def rerank(query, docTexts, docIDs):
 
     docs = [docTexts(id) for id in docIDs]
 
+    #Encodes documents and query using SBERT and determines the similarity pairings
     docEmbeddings = model.encode(docs)
     queryEmbeddings = model.encode(query)
     scores = util.cos_sim(queryEmbeddings, docEmbeddings)[0].numpy()
 
+    #Sorts the results by descending similarity score
     ranked = [docIDs[i] for i in np.argsort(-scores)]        
     
     return ranked
